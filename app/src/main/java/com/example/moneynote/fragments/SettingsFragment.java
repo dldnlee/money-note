e1b86102a5c663d454f48122cca6a2d6e4b70d13
24makiyuka <70732444+24makiyuka@users.noreply.github.com>
2021-12-10 14:54:57 +0900

    settingpage,alarm

--- app/src/main/java/com/example/moneynote/fragments/SettingsFragment.java
+++ app/src/main/java/com/example/moneynote/fragments/SettingsFragment.java
@@ -1,8 +1,14 @@
 package com.example.moneynote.fragments;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 
 import android.util.Log;
@@ -11,13 +17,20 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.example.moneynote.R;
 import com.example.moneynote.databinding.FragmentExpenseBinding;
 import com.example.moneynote.databinding.FragmentHomeBinding;
 import com.example.moneynote.databinding.FragmentSettingsBinding;
 
 
+
+import com.example.moneynote.databinding.FragmentSettingsBinding;
+
+
 public class SettingsFragment extends Fragment {
     private FragmentSettingsBinding binding;
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
 
     @Override
     public View onCreateView(LayoutInflater inflater,
@@ -28,6 +41,7 @@ public class SettingsFragment extends Fragment {
 
 
         return binding.getRoot();
+
     }
 
 }
